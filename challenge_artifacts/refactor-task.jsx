33ccd6bc3467b7159f96094486db8bd4f91ff06d
Lakshaya Sood <lakshaya.sood@infosys.com>
2019-12-11 19:44:34 +0100

    ♻️chore(artifacts): add code challenge atifacts

---
+++ challenge_artifacts/refactor-task.jsx
@@ -0,0 +1,28 @@
+/**
+ * ----------------------------------------
+ * refactoring
+ * ----------------------------------------
+ */
+
+/**
+ * ----------------------------------------
+ * refactor work function
+ * - spot the code smell(s)
+ * - rewrite the function until you think it's much better
+ * - you are free to do ANYTHING
+ * ----------------------------------------
+ */
+const work = obj => {
+  if (obj.type === 'user') {
+    return userClient.get(`/users/${obj.id}`);
+  } else if (obj.type === 'record') {
+    return recordClient.post('/url/to/another/service/records', obj);
+  } else if (obj.type === 'log') {
+    if (obj.allowWrite) {
+      localStorage.write(obj);
+      remoteStorage.write(obj);
+    } else {
+      console.log(obj);
+    }
+  }
+};
